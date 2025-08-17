592689ff90cecbc37a4ff3f83e84a89c1e4f99f6
david11133 <davidnady4yad@gmail.com>
2025-08-17 13:41:52 +0300

    handle indices_pq for each dataset

--- crocolaketools/test/test_data.py
+++ crocolaketools/test/test_data.py
@@ -44,12 +44,15 @@ class TestData:
         if np.issubdtype(selected_scalar.dtype, np.datetime64):
             return selected_scalar.values
         elif isinstance(selected_scalar.item(), bytes):
-            return np.datetime64(
-                datetime.strptime(
-                    selected_scalar.item().decode(),
-                    "%Y%m%d%H%M%S"
+            try:
+                return np.datetime64(
+                    datetime.datetime.strptime(
+                        selected_scalar.item().decode(), 
+                        "%Y%m%d%H%M%S"
+                    )
                 )
-            )
+            except ValueError: # handle 'byte' data that isn't datetime format
+                return selected_scalar.item().decode()
         else:
             return selected_scalar.item()
 
@@ -190,18 +193,16 @@ class TestData:
         params_crocolake2db = params.params[ "CROCOLAKE2" + db_name]
         lat_name = params_crocolake2db["LATITUDE"]
         lon_name = params_crocolake2db["LONGITUDE"]
-        juld_name = params_crocolake2db["JULD"]
-        depth_name = params_crocolake2db["DEPTH"]
 
         for j in range(1000):
             nc_file = random.choice( nc_files )
 
             if db_name == "Argo":
                 ds = xr.open_dataset(nc_file, engine="argo")
-            elif db_name == "SprayGliders":
-                ds = xr.open_dataset(nc_file, engine="h5netcdf")
-            else:
+            elif db_name in ["Oleander", "Saildrones"]:
                 ds = xr.open_dataset(nc_file, engine="netcdf4")
+            else:
+                ds = xr.open_dataset(nc_file, engine="h5netcdf")
 
             #only test variables that are preserved in crocolake
             ds_vars = list(ds.data_vars)
@@ -216,9 +217,8 @@ class TestData:
                 # Exclude coordinate variables ('latitude', 'longitude', 'time') for Saildrones
                 # since they do not have corresponding depth information, which is required 
                 # for uniquely identifying each row in the dataset.
-                excluded_vars = {lat_name, lon_name, juld_name}
+                excluded_vars = {lat_name, lon_name, "time"}
                 variables = [v for v in variables if v not in excluded_vars]
-
 
             logging.info(f"variables:{variables}")
             random_var = random.choice(variables)
@@ -257,22 +257,23 @@ class TestData:
             var_pq = params_db2crocolake[random_var]
             cols_pq = [var_pq]
             indices_pq = {}
-            # ensure we retrieve a unique row for each dataset
             for k, v in indices.items():
                 if k in params_db2crocolake:
                     indices_pq[ params_db2crocolake[k] ] = self._get_scalar_from_ds(ds[k][v])
+                else:
+                    if db_name == "Oleander":
+                        nc_depth = self._get_scalar_from_ds(ds["depth"].isel(**indices))
+                        indices_pq[ "DEPTH" ] = nc_depth
+                    elif db_name == "Saildrones":
+                        depth_map = params.params["Saildrones_depth_map"]
+                        nc_depth = depth_map[random_var]
+                        nc_juld = self._get_scalar_from_ds(ds["time"].isel(**indices))
+                        indices_pq[ "DEPTH" ] = np.float32(nc_depth)
+                        indices_pq[ "JULD" ] = nc_juld
             indices_pq[ "LATITUDE" ] = nc_lat
             indices_pq[ "LONGITUDE" ] = nc_lon
             if db_name_config != "ARGO-GDAC":
                 indices_pq[ "LONGITUDE" ] = (indices_pq[ "LONGITUDE" ] - 180) % 360 - 180
-            if db_name == "Saildrones":
-                depth_map = params.params["Saildrones_depth_map"]
-                if random_var in depth_map:
-                    nc_depth = depth_map[random_var]
-                    indices_pq["DEPTH"] = np.float32(nc_depth)
-                juld_value = self._get_scalar_from_ds(ds[juld_name].isel(**indices))
-                indices_pq["JULD"] = juld_value
-
             cols_pq.extend(indices_pq.keys())
 
             logging.info(f"var_pq: {var_pq}")
@@ -289,11 +290,16 @@ class TestData:
                                 # (if the was missing and the whole row it ended
                                 # up into contained missing data that was thus
                                 # discarded)
-
             if ddf.shape[0] == 0:
                 # if the original data ended in a row with all observations as
                 # pd.NAs the row was dropped as it did not contain relevant info
                 logging.info("pq_value was pd.NA and discarded")
+
+                if isinstance(nc_value, (float, int)) and nc_value < -1e20:
+                    # some missing data might be stored as extremely large negative num
+                    # (e.g, -9.999900276792041e+20). that should be treated as missing
+                    nc_value = np.nan
+                    
                 assert pd.isna(nc_value)
                 continue
 
@@ -323,7 +329,6 @@ class TestData:
                 elif np.issubdtype(type(pq_value), np.floating):
                     pq_value = np.float32(pq_value)
                     nc_value = np.float32(nc_value)
-                # For Timestamp objects, compare directly
                 assert pq_value == nc_value
 
             else:
@@ -360,6 +365,13 @@ class TestData:
         )
 
 #------------------------------------------------------------------------------#
+    def test_data_integrity_oleander_phy(self):
+        self._check_variables_nc(
+            db_type="PHY",
+            db_name="Oleander"
+        )
+
+#------------------------------------------------------------------------------#
     def test_data_integrity_argogdac_phy(self):
         self._check_variables_nc(
             db_type="PHY",
@@ -392,17 +404,10 @@ class TestData:
         )
 
 #------------------------------------------------------------------------------#
-    def test_profiles_saildrones_phy(self):
+    def test_profiles_oleander_phy(self):
         self._check_profiles(
             db_type="PHY",
-            db_name="Saildrones",
-        )
-
-#------------------------------------------------------------------------------#
-    def test_profiles_saildrones_bgc(self):
-        self._check_profiles(
-            db_type="BGC",
-            db_name="Saildrones",
+            db_name="Oleander"
         )
 
 #------------------------------------------------------------------------------#
