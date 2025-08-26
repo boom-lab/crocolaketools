9d98aeaaf80d7886cd9cb8709cd019c181197a09
david11133 <davidnady4yad@gmail.com>
2025-08-26 19:09:34 +0300

    Comment out the command in oleander_dl and explain that the user need to provide a url.txt file

--- scripts/oleander_dl.sh
+++ scripts/oleander_dl.sh
@@ -6,6 +6,8 @@ download_oleander \
     --save_to ./oleander_data/ \
     --threads 8 
 
+# To use download_oleander with a list of URLs, provide your own oleander.txt file
+# This file should contain one URL per line
 download_oleander \
     --url_file oleander.txt \
     --save_to ./oleander_data/ \
