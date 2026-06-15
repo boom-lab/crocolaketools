a9ec7b1d496c1b4fc07f8833fa7c8ff0963c94e1
Mahi <anol.mahi@gmail.com>
2026-06-15 23:13:41 +0600

    :file-conventions-fixed

--- crocolaketools/db_names.py
+++ crocolaketools/db_names.py
@@ -7,5 +7,11 @@
 # Source of truth lives here in CrocoLakeTools; the copy in CrocoLakeLoader is
 # Will be kept in sync via CI/CD ( details at boom-lab/crocolaketools#52 and
 # boom-lab/crocolakeloader#9).
+#
+## @author Mahi Sarwar Anol <anol.mahi@gmail.com> 
+#
+## Everything is Adapted from params.py in crocolakeloader (https://github.com/boom-lab/crocolakeloader.git)
+#  Originally written by enrico <enrico.milanese@whoi.edu>
+
 
 databases = ["ARGO", "GLODAP", "SprayGliders", "CPR", "Saildrones", "OleanderXBT", "IOOS_GLIDERS"]
